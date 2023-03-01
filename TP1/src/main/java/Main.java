3b990d938f33dcda05a32786ce84c2dc0e1b7157
AC-climoilou <1701633@cegeplimoilou.ca>
2023-03-01 16:45:21 -0500

    Début Main

--- TP1/src/main/java/Main.java
+++ TP1/src/main/java/Main.java
@@ -1,8 +1,18 @@
 public class Main {
 
     public static void main(String args[]) {
+        // Initialisation du paquet
         Paquet paquet = new Paquet();
         paquet.initPaquet();
+
+        // Initialisation du banquier et joueur
+        Joueur banquier = new Joueur();
+        Joueur joueur = new Joueur();
+
+        // Initialisation de la partie
+        Partie partie = new Partie();
+
+
 
     }
 }
